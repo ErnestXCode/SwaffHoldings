ff4f3316b6f846a7e99c86c63284a9209c819b4d
Ernest <enkaranu58@gmail.com>
2026-08-14 09:20:10 +0300

    init

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import { ThemeProvider } from "../context/ThemeContext";
 
 function MainLayout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <ThemeProvider>
       <div className="flex min-h-screen flex-col bg-slate-50 dark:bg-slate-950">
